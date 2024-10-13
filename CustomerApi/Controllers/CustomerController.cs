4b9ccc59f21cbb32031946a1f4bb4dfdbfc42649
Jeevitha <gvtha98@gmail.com>
2024-10-13 16:05:05 +0530

    Customer Application Api

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,81 @@
+﻿using Core.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerService _customerService;
+
+        public CustomerController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("GetCustomers")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var customers = await _customerService.GetAllCustomersAsync();
+            return Ok(customers);
+        }
+
+        [HttpGet("GetCustomer/{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer == null) return NotFound();
+            return Ok(customer);
+        }
+
+        [HttpPost("CreateCustomer")]
+        public async Task<ActionResult> CreateCustomer(Customer customer)
+        {
+            await _customerService.AddCustomerAsync(customer);
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+        }
+
+        [HttpPut("UpdateCustomer/{id}")]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id) return BadRequest();
+            await _customerService.UpdateCustomerAsync(customer);
+            return NoContent();
+        }
+
+        [HttpDelete("DeleteCustomer/{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            await _customerService.DeleteCustomerAsync(id);
+            return NoContent();
+        }
+
+        [HttpPatch("PatchCustomer/{id}")]
+        public async Task<IActionResult> PatchCustomer(int id, [FromBody] Customer customerDto)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound($"Customer with ID {id} not found");
+            }
+            if (customerDto.FirstName != null)
+            {
+                customer.FirstName = customerDto.FirstName;
+            }
+            if (customerDto.LastName != null)
+            {
+                customer.LastName = customerDto.LastName;
+            }
+            if (customerDto.Email != null)
+            {
+                customer.Email = customerDto.Email;
+            }
+
+            await _customerService.UpdateCustomerAsync(customer);
+            return NoContent();
+        }
+    }
+}
---
+++ Infrastructure/Repository/CustomerRepository.cs
@@ -0,0 +1,54 @@
+﻿using Core.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CustomerDbContext _context;
+
+        public CustomerRepository(CustomerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCustomerAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCustomerAsync(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
